321fc44e13cd3673d4117ec69efe7e9b3d224004
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2023-11-16 13:41:42 +0000

    radix sort algo implemented

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -9,13 +9,51 @@
 
 */
 
-function radixSort(array) {
-  // code goes here
+const getMaxDigits = (nums) => {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, nums[i].toString().length);
+  }
+  return maxDigits;
 }
+
+const getDigit = (number, place, max) => {
+  const string = number.toString();
+  const size = string.length;
+
+  const mod = max - size;
+  return string[place - mod] || 0;
+}
+
+// console.log(getDigit(1000000, 0));
+
+function radixSort(array) {
+  let maxDigits = getMaxDigits(array);
+  let n = maxDigits;
+
+  while (maxDigits > 0) {
+    let buckets = new Array(10).fill().map(() => []);
+
+    for (let i = 0; i < array.length; i++) {
+      // console.log(getDigit(array[i], maxDigits - 1));
+      const val = array[i]
+      buckets[getDigit(val, maxDigits - 1, n)].push(val);
+    }
+
+    // console.log(buckets);
+
+    array = [].concat(...buckets);
+    maxDigits--;
+  }
+
+  return array;
+}
+
+// console.log(radixSort([10000000, 100000, 1000, 100, 10, 1]))
 
 // unit tests
 // do not modify the below code
-describe.skip("radix sort", function () {
+describe("radix sort", function () {
   it("should sort correctly", () => {
     const nums = [
       20,
@@ -71,6 +109,6 @@ describe.skip("radix sort", function () {
       .fill()
       .map(() => Math.floor(Math.random() * 500000));
     const ans = radixSort(nums);
-    expect(ans).toEqual(nums.sort());
+    expect(ans).toEqual(nums.sort((a, b) => a - b));
   });
 });
